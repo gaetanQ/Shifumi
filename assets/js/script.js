90260be35c6f32b5da1e6a6b519c05944ee62114
gaetanQ <querolle.gaetan@gmail.com>
2020-05-13 16:17:52 +0200

    draggable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,48 @@
-$( function() {
+$(function(){
+
+  $("#pierre").draggable({
+   
+  });
+  $("#feuille").draggable({
+  });
+  $("#ciseaux").draggable({
+
+  });
+        $("#drop1").droppable({
+
+          accept : '#pierre',
+          
+            drop: function(event, ui) {
+              
+                $(this).css('background', 'rgb(0,200,0)');
+            },
+            over: function(event, ui) {
+                $(this).css('background', 'orange');
+            },
+            out: function(event, ui) {
+                $(this).css('background', 'cyan');
+            }
+        });
+        $("#playAgain").click(function(){
+          
+
+        });
+      });
+
+
+
+
+
+/*
     $("#pierre").draggable();
     $("#feuille").draggable();
     $("#ciseaux").draggable();
     $( "#drop1" ).droppable({
         drop: function( event, ui ) {
         $( this )
-              .addClass( "widget" )
-              .find( "img" )
+              .addClass( "widget" );
+              .find( "img" );
                 .html( "Dropped!" );
+                
 });
+    }); */
