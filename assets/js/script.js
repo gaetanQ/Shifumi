97a71391a84c3ca30f29ed6c1494067c826b03dc
fabmoulin <fabienne.moulinpro@gmail.com>
2020-05-14 15:07:16 +0200

    maj script suit point Chloé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,50 @@
-$(function(){.
-  var widgetValues = ['pierre', 'feuille', 'ciseaux'];
-  var valueComputer = Math.floor(Math.random() * widgetValues.length);
-  console.log(valueComputer);
+$(function(){ //ouverture de fonction principale
+  var arrayChoices = ['pierre', 'feuille', 'ciseaux']; // Je crée un tableau avec les choix possible de l'ordinateur
+  var gamerChoice = null; // Je définis une variable pour le choix du joueur et l'initialise à null. L'idée c'est qu'elle existe au chargement de ma page
+  var message ="";
 
-  $("#0").draggable({
-   
+  $(".widget").draggable({  // Je rend déplaçable tous les éléments qui ont la classe choice
+    revert : true, //Renvoie l'élément toujours à sa place
+    snap:"#droppable";//Elles sont attirées par le bloc droppable
+    snapMode: 'inner'; // Permet le magnétisme sur l'interieur du block
   });
-  $("#1").draggable({
-  });
-  $("#2").draggable({
 
+  // J'ajoute un évènement mouseup à mes éléments
+  // En fonction de l'image choisi j'attribue une valeur au choix du joueur
+  $('#rockChoice').mouseup(function(){
+    gamerChoice = 0;
+    // Je vérifie dans ma console ce qui est stocker dans la variable gamerChoice
+    console.log('Choix du joueur' + gamerChoice);
   });
-  
-  $("#drop1").droppable({
-
-          //accept : '#pierre', '#feuille', '#ciseaux';
-          
-            drop: function(event, ui) {
-              
-                $(this).css('background', 'rgb(0,200,0)');
-            },
-            over: function(event, ui) {
-                $(this).css('background', 'orange');
-            },
-            out: function(event, ui) {
-                $(this).css('background', 'cyan');
-            }
+  $('#paperChoice').mouseup(function(){
+    gamerChoice = 1;
+    console.log('Choix du joueur' + gamerChoice);
+  });
+  $('#scissorsChoice').mouseup(function(){
+    gamerChoice = 2;
+    console.log('Choix du joueur' + gamerChoice);
   });
   
-  //$("#playAgain").click(function(){
-          
 
-  });
+    $( "#droppable" ).droppable({// Je rend ma div droppable
+        accept: '.answer',// Elle n'accepte que les éléments qui ont la classe answer
+        drop : function(event, ui){ //Je lui ajoute l'évènement jqueryUI drop. // Au moment de l'évènement droppable
+          var computerChoice = Math.floor(Math.random() * arrayChoices.length); // L'ordinateur choisi aléatoirement un choix dans le tableau définis précédemment
+          console.log('Choix de l\'ordinateur' + computerChoice);
 
-  $('#drop1').droppable(function() {
-    var valueGamer = parseFloat($(this).val());
-    var valueComputer = [Math.floor(Math.random() * widgetValues.length)];
-    var message = "";
+        }
+    });
+
+
+   //MAINTENANT JE DOIS COMPARER LES CHOIX DE L'ORDINATEUR ET CELUI DU JOUEUR POUR AFFICHER LES BONS MESSAGES
   
-  if (valueGamer == valueComputer) {
+  if (gamerChoice == computerChoice) {
       message = "Égalité !";
   }
   else if (
-      (valueGamer == "pierre" && valueComputer == "ciseau")
-      || (valueGamer == "feuille" && valueComputer == "pierre")
-      || (valueGamer == "ciseau" && valueComputer == "feuille")
+      (gamerChoice == "pierre" && computerChoice == "ciseaux")
+      || (gamerChoice == "feuille" && computerChoice == "pierre")
+      || (gamerChoice == "ciseaux" && computerChoice == "feuille")
   ) {
       message = "Gagné !";
       //your_win++;
@@ -54,24 +54,14 @@ $(function(){.
       //opponent_win++;
   }
 
-   //if l'ordinateur a choisi la valeur pierre alors j'affiche "append" l'image pierre (au lieu d'afficher une alerte j'affiche l'image)
-});
-
-
-   
-
-
-
-/*
-    $("#pierre").draggable();
-    $("#feuille").draggable();
-    $("#ciseaux").draggable();
-    $( "#drop1" ).droppable({
-        drop: function( event, ui ) {
-        $( this )
-              .addClass( "widget" );
-              .find( "img" );
-                .html( "Dropped!" );
-                
-});
-    }); */
+  $('#drop2').droppable(function() {
+        if (computerChoice == "#0") {
+        $("<img>#0</img>").appendTo("#0")}
+    
+        else if (computerChoice == "#1") {
+        $("img").appendTo("#1")}
+    
+        else if (computerChoice == "#2") {
+        $("img").appendTo("#2")}
+  
+}); //fermeture de la fonction principale
